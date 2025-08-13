2a74019238b4d7960f1897f492534bdab6d8888b
翱翔的雄库鲁 <andywebjava@163.com>
2025-08-14 02:05:53 +0800

    Delete AI/app directory

--- AI/app/base/exception.py
+++
@@ -1,9 +0,0 @@
-class AIManagerError(RuntimeError):
-    def __init__(self, code, msg):
-        super().__init__(msg)
-        self.code = code
-        self.msg = msg
-
-class SystemInternalError(AIManagerError):
-    def __init__(self, msg):
-        super().__init__(-1, msg)
--- AI/app/base/scheduler.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-import tzlocal
-from apscheduler.schedulers.background import BackgroundScheduler
-
-scheduler = BackgroundScheduler(timezone=tzlocal.get_localzone_name())
-logging.getLogger('apscheduler').setLevel(logging.ERROR)
